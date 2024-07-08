eb185dd3015f7da6a31b168bf1bb0723b143c170
Vonstryker <josemarti@my.tridenttech.edu>
2024-07-08 19:48:01 -0400

    Update lab4-3.cpp

--- lab4-3.cpp
+++ lab4-3.cpp
@@ -7,13 +7,16 @@ using namespace std;
 int main()
 {
   //declare named constant and variables
-  const double COMM_RATE = 0.1;
+  double COMM_RATE = 0.1;
   double sales = 0.0;
   double commission = 0.0;
 
   //enter input item
   cout << "Sales amount: ";
   cin >> sales;
+
+  cout << "commission rate: ";
+  cin >> COMM_RATE;
 
   //calculate and display the commision
   commission = sales * COMM_RATE;
