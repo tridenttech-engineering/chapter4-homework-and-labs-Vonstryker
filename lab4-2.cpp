e86e5f906d6f948f77abf241a591b9bfa3b3d096
Vonstryker <josemarti@my.tridenttech.edu>
2024-07-08 19:10:41 -0400

    Update lab4-2.cpp

--- lab4-2.cpp
+++ lab4-2.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 int main()
 {
+  const double COMM_RATE= 0.1;
+  double sales= 0.0;
+  double commission = 0.0;
+  cout << "Sales amount: ";
+  cin >> sales;
+  commission = sales * COMM_RATE;
+  cout << "Commission: $;
+    << commission << endl;
 
 
   return 0;
