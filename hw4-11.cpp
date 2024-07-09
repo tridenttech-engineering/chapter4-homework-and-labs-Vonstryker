73005776f3e0ae1ef2a4d1f8cb24dd456eebaa55
Vonstryker <josemarti@my.tridenttech.edu>
2024-07-08 20:11:04 -0400

    Update hw4-11.cpp

--- hw4-11.cpp
+++ hw4-11.cpp
@@ -5,7 +5,20 @@
 using namespace std;
 
 int main() {
-  
+  double salePrice = 0.0;
+  double taxRate = 0.0;
+
+  double salesTax = 0.0;
+
+  double cost = 0.0;
+
+  cout << "enter the sale price: ";
+  cin >> salePrice;
+  cout << "Enter the sales tax rate: "
+  cin >> taxRate;
+  salesTax = salePrice * taxRate;
+  cost = salePrice + salesTax;
+  cout << "Cost: $" << cost << endl;
 
   return 0;
 } // end of main function
