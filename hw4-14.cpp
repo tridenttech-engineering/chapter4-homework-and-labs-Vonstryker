5601cae06e40ddd1bd984f7f7b9f3be1696ea8dc
Jose Marti <josemarti@my.tridenttech.edu>
2024-07-09 18:52:06 -0400

    Update hw4-14.cpp

--- hw4-14.cpp
+++ hw4-14.cpp
@@ -5,7 +5,21 @@
 using namespace std;
 
 int main() {
-  
+  double smallPizza = 0.0;
+  double mediumPizza = 0.0;
+  double largePizza = 0.0;
+  double familyPizza = 0.0;
 
+  cout << "smallPizza: ";
+  cin >> smallPizza;
+  cout << "mediumPizza: ";
+  cin >> mediumPizza;
+  cout << "largePizza: ";
+  cin >> largePizza;
+  cout << "familyPizza: ";
+  cin >> familyPizza;
+
+
+  
   return 0;
 } // end of main function
